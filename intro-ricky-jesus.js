199dc8b4867ebc28905339953b9d2d22a6880d37
AJCodes1 <etsncamacho@gmail.com>
2023-05-10 15:38:09 -0700

    intro challenges

---
+++ intro-ricky-jesus.js
@@ -0,0 +1,52 @@
+// // Write the code that will log the outcome of 34 added to 71.
+console.log(34 + 71) // 105
+// // Write the code that will log the outcome of 67 subtracted from 123.
+ console.log(67 + 123) // -56
+// // Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56 * 23) // 1288
+// // Write the code that will log the outcome of 45 divided by 5.
+console.log(45 / 5) // 9
+// // Write the code that will log the outcome of 5 to the power of 7.
+console.log(5 ** 7) //78125
+// // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6) // 3
+// // Write the code that will log the length of a string containing your name.
+const names = "ricky_jesus"
+console.log(names.length) //11
+// // Write the code that will log whether your string includes the letter "e"?
+console.log(names.includes("e")) // true
+// // Write the code that will log the character at the first index of the string.
+console.log(names[0]) //r
+// // Write the code that will log the string in all uppercase letters.
+console.log(names.toUpperCase()) // RICKY_JESUS 
+
+
+const theQuestion = "life, the universe, and everything"
+const theAnswer = 42
+
+// Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2)
+// Write the code that will log the whole number remainder of theAnswer when divided by 3.
+console.log(theAnswer % 3)
+// Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
+// Write the code that will log the length of theQuestion divided by 2.
+console.log(theQuestion.length / 2)
+// Write the code that will log the length of theQuestion added to theAnswer.
+console.log(theQuestion.length + theAnswer)
+// Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion[2])
+// Write the code that will log the index of the second comma in the theQuestion.
+console.log(theQuestion[18])
+// Write the code that will log the concatenation of the two variables.
+console.log(theQuestion + theAnswer)
+// Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion.charAt(0))
+// Write the code that will extract "the universe" from theQuestion.
+console.log(theQuestion.slice(6, 18))
+// Write the code that will extract "everything" from theQuestion.
+console.log(theQuestion.slice(24, 34))
+// Write the code that will log the last character of theQuestion without manually counting the number of characters.
+console.log(theQuestion.charAt(theQuestion.length - 1))
+
+
