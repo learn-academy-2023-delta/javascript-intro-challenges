a764a374d7d7affc61d046d0bfc166276a4f993e
kmcarbajal <carbz2211@gmail.com>
2023-05-10 11:41:00 -0700

    Finishedchallenge questions.

--- javascript-kevin-justin.js
+++ javascript-kevin-justin.js
@@ -9,7 +9,13 @@ console.log(45/5)
 // Write the code that will log the outcome of 5 to the power of 7.
 console.log(5**7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6)
 // Write the code that will log the length of a string containing your name.
+console.log("Kevin".length)
 // Write the code that will log whether your string includes the letter "e"?
+console.log("Kevin".includes("e"))
+console.log("Kevin".toLowerCase().includes("e".toLowerCase()))
 // Write the code that will log the character at the first index of the string.
+console.log("Kevin".charAt(0))
 // Write the code that will log the string in all uppercase letters.
+console.log("Kevin".toUpperCase())
