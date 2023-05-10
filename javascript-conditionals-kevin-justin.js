ea1147de80a0bc915084c3f1ac272da537116c1c
kmcarbajal <carbz2211@gmail.com>
2023-05-10 15:57:41 -0700

    Finished up!

--- javascript-conditionals-kevin-justin.js
+++ javascript-conditionals-kevin-justin.js
@@ -18,3 +18,66 @@ console.log("LEARN".includes("rn"))
 console.log("LEARN"[0]=== "l")
 // Modify the code from the previous question to return true.
 console.log("LEARN".toLowerCase()[0]=== "l".toLowerCase())
+
+// Make sure you try different options and change the variables to ensure properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+const item1 = 101
+
+if(item1 <= 100) {
+    console.log("in budget")
+} else {
+    console.log("too expensive!")
+}
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+const hungry = 1
+
+if(hungry === 1) {
+    console.log("eat food")
+} else {
+    console.log("keep coding")
+}
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+const trafficLight = "blue"
+
+if(trafficLight === "green") {
+    console.log("go")
+} else if(trafficLight === "yellow") {
+    console.log("slow down")
+} else if(trafficLight === "Red") {
+    console.log("stop")
+} else {
+    console.log("Oops! Something seems fishy..")
+}
+    
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
+var num1 = 16
+var num2 = 32
+
+if(num1 > num2) {
+    console.log(num1)
+} else if(num2 > num1) {
+    console.log(num2)
+} else if(num1 === num2) {
+    console.log("the numbers are the same")
+} else {
+    console.log("Oops! Something seems fishy..")
+}
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+var number1 = 10
+
+if(number1 % 2 === 0) {
+    console.log("the number is even!")
+} else if (number1 % 2 !== 0) {
+    console.log("the number is odd!")
+} else if(number1 === 0) {
+    console.log("the number is zero!")
+}
