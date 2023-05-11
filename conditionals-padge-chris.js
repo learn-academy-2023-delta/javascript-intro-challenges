4893c023073e78b875f2079d644eb32c039f637d
PadgeM <padge.mattaliano@gmail.com>
2023-05-11 15:27:22 -0700

    Arrays

--- conditionals-padge-chris.js
+++ conditionals-padge-chris.js
@@ -79,3 +79,4 @@ if(num3 % 2 > 0 || num3 % 2 < 0 ){
 // Write a statement that takes a variable of a boolean, number, or string data type and logs the data type of the variable. HINT: Check out the JavaScript typeof operator.
 
 // Create a password checker using a single conditional statement. If a user inputs a password with 12 or more characters AND the password includes !, then log "That is a mighty strong password!" If the user’s password is 8 or more characters OR includes !, then log "That password is strong enough." Log "That is not a valid password." for every other input.
+
